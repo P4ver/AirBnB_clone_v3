24e18afc84416ac5bac1252c69f0c424b4e1ebad
P4ver <s.pales20@gmail.com>
2024-04-02 20:56:06 +0100

    edit

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,3 +13,20 @@ def api_status():
     Returns the status of the API.
     """
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats')
+def get_stats():
+    """
+    Returns the stats of the API.
+    """
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'users': storage.count('Users'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'cities': storage.count('City'),
+    }
+
+    return jsonify(stats)
