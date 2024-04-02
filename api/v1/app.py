201efd56518330f6f456bad835eab7868f4c241e
P4ver <s.pales20@gmail.com>
2024-04-02 23:58:27 +0100

    tsk

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 Creates Flask app,
 '''
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -11,8 +11,17 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+@app.teardown_appcontext
+def teardown_eng(exception):
+    """teardown"""
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """404 error handler"""
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == '__main__':
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
     PORT = int(getenv('HBNB_API_PORT', '5000'))
-    app.run(host=HOST, port=PORT, threaded=True)
+    app.run(debug=True , host=HOST, port=PORT, threaded=True)
